fbc219c62efec0c70e6bfdf9d5807a41bed8e39f
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-10-11 13:54:47 +0200

    probably nothing

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -42,7 +42,9 @@ async function getPhotographerById(id) {
 
     const data = await response.json();
     const photographer = data.photographers.find(photographer => photographer.id === parseInt(id, 10));
-
+    const photographerPhotos = data.media.filter(photo => photo.photographerId === parseInt(photographerId));
+    console.log(photographerPhotos);
+    createPhotoGrid(photographerPhotos);
     if (!photographer) {
       throw new Error('Photographer not found');
     }
@@ -52,4 +54,50 @@ async function getPhotographerById(id) {
     console.error('Error fetching photographer data:', error);
     throw error;
   }
+}
+
+function createPhotoGrid(photos) {
+  const photoGrid = document.querySelector('.photo-grid');
+  photoGrid.innerHTML = ''; // Clear previous content
+
+  photos.forEach(photo => {
+    const photoDiv = document.createElement('div');
+    photoDiv.classList.add('photo');
+
+    const image = document.createElement('img');
+    image.src = `assets/images/${photographerId}/${photo.image}`; // Include photographerId in the path
+    image.alt = photo.title;
+
+    const photoInfo = document.createElement('div');
+    photoInfo.classList.add('photo-info');
+
+    const title = document.createElement('h3');
+    title.textContent = photo.title;
+
+    const likes = document.createElement('p');
+    likes.textContent = `${photo.likes}`;
+    likes.classList.add('photo-likes');
+
+
+    const heart = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    heart.setAttribute("width", "21");
+    heart.setAttribute("height", "24");
+    heart.setAttribute("viewBox", "0 0 21 24");
+    heart.setAttribute("fill", "none");
+    const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    g.setAttribute("clip-path", "url(#clip0_120_561)");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M10.5 21.35L9.23125 20.03C4.725 15.36 1.75 12.28 1.75 8.5C1.75 5.42 3.8675 3 6.5625 3C8.085 3 9.54625 3.81 10.5 5.09C11.4537 3.81 12.915 3 14.4375 3C17.1325 3 19.25 5.42 19.25 8.5C19.25 12.28 16.275 15.36 11.7688 20.04L10.5 21.35Z");
+    path.setAttribute("fill", "#911C1C");
+    g.appendChild(path);
+    heart.appendChild(g);
+
+
+    photoInfo.appendChild(heart);
+    photoInfo.appendChild(title);
+    photoInfo.appendChild(likes);
+    photoDiv.appendChild(image);
+    photoDiv.appendChild(photoInfo);
+    photoGrid.appendChild(photoDiv);
+  });
 }
---
+++ scripts/utils/dropdown.js
@@ -0,0 +1,37 @@
+const dropdown = document.querySelector('.dropdown');
+const selected = dropdown.querySelector('.selected');
+const options = dropdown.querySelector('.dropdown-menu');
+const dropdownIcon = dropdown.querySelector('.dropdown-icon');
+
+selected.addEventListener('click', () => {
+    toggleDropdown();
+});
+
+selected.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        toggleDropdown();
+    }
+});
+
+options.addEventListener('click', (e) => {
+    if (e.target.classList.contains('dropdown-option')) {
+        const selectedText = selected.textContent;
+        selected.textContent = e.target.textContent;
+        e.target.textContent = selectedText;
+        selected.setAttribute('aria-label', `Selected option: ${e.target.textContent}`);
+        selected.setAttribute('aria-expanded', 'false');
+        options.style.display = 'none';
+        
+        // Reset the icon rotation
+        dropdownIcon.style.transform = 'rotate(0)';
+    }
+});
+
+// Function to toggle the dropdown
+function toggleDropdown() {
+    options.style.display = options.style.display === 'block' ? 'none' : 'block';
+    selected.setAttribute('aria-expanded', options.style.display === 'block' ? 'true' : 'false');
+    
+    // Rotate the icon
+    dropdownIcon.style.transform = options.style.display === 'block' ? 'rotate(90deg)' : 'rotate(0)';
+}
