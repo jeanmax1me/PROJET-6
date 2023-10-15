b13c2244ff4213aab5543dae5558bfd0ed3ae354
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-10-15 17:37:50 +0200

    input validation

--- scripts/pages/modal.js
+++ scripts/pages/modal.js
@@ -43,6 +43,86 @@ const form = document.getElementById('contact-form');
 
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-    closeModal();
+  event.preventDefault(); 
+   validate();
   });
+
+
+  // FORM VALIDATION 
+
+function validateName(input) {
+  const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/;
+  if (!regex.test(input.value)) {
+    input.classList.add("field-error");
+    return false;
+  } else {
+    input.classList.remove("field-error");
+    return true;
+  }
+}
+
+function validateEmail(input) {
+  const emailRegex = /^[A-Za-z]{1,}[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
+  if (!emailRegex.test(input.value)) {
+    input.classList.add("field-error");
+    return false;
+  } else {
+    input.classList.remove("field-error");
+    return true;
+  }
+}
+
+
+function validateMessage(input) {
+  if (input.value === "") {
+    input.classList.add("field-error");
+    return false;
+  } else {
+    input.classList.remove("field-error");
+    return true;
+  }
+}
+
+// FORM VALIDATION PROCESS
+
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+//EVENTS LISTENERS ON CHANGE
+firstNameInput.addEventListener("input", function() {
+  validateName(firstNameInput);
+});
+lastNameInput.addEventListener("input", function() {
+  validateName(lastNameInput);
+});
+emailInput.addEventListener("input", function() {
+  validateEmail(emailInput);
+});
+messageInput.addEventListener("input", function() {
+  validateMessage(messageInput);
+});
+
+
+  function validate() {
+    const firstNameValid = validateName(firstNameInput);
+    const lastNameValid = validateName(lastNameInput);
+    const emailValid = validateEmail(emailInput);
+    const messageValid = validateMessage(messageInput);
+
+  
+    const isValid =
+      firstNameValid &&
+      lastNameValid &&
+      emailValid &&
+      messageValid;
+
+    if (isValid) {
+      event.preventDefault();
+      closeModal();
+      return true;
+    } else {
+      return false;
+    }
+  }
